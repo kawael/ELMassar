a1b385584849a55eb270e97c8fc6b87a8e62d51d
wael <wail.chaabane@gmail.com>
2025-04-29 16:12:47 +0100

    init work

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
 import cors from 'cors';
+import fs from 'fs';
 
 const app = express();
 
@@ -20,17 +21,29 @@ app.get('/', (req, res) => {
 }
 );
 
+const dataFilePath = './data.json';
+
 io.on('connection', (socket) => {
     console.log('a user connected');
 
+    // Emit data from JSON file
+    fs.readFile(dataFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading JSON file:', err);
+            return;
+        }
+        const jsonData = JSON.parse(data);
+        socket.emit('target-data', jsonData);
+    });
+
     socket.on('send-location', (data) => {
-        socket.emit('receive-location', {id: socket.id, ...data});
+        socket.emit('receive-location', { id: socket.id, ...data });
     });
 
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+        console.log('user disconnected');
     });
-  });
+});
 
 server.listen(3000, () => {
     console.log('listening on PORT:3000');
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { io } from "socket.io-client";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Polyline } from "react-leaflet";
 const socket = io("http://localhost:3000/")
 
 
@@ -26,6 +26,7 @@ import { useState } from "react";
 
 export default function App() {
   const [markers, setMarkers] = useState([]);
+  const [lines, setLines] = useState([]); // State to store lines
 
   socket.on("receive-location", (data) => {
     const { id, latitude, longitude } = data;
@@ -34,9 +35,12 @@ export default function App() {
       const newMarkers = { ...prevMarkers };
       newMarkers[id] = { latitude, longitude };
       return newMarkers;
-    }
-    );
+    });
+  });
 
+  socket.on("target-data", (data) => {
+    const { coordinates } = data; // Assuming coordinates is an array of [latitude, longitude] pairs
+    setLines((prevLines) => [...prevLines, coordinates]);
   });
 
   socket.on("disconnect", (data) => {
@@ -45,17 +49,13 @@ export default function App() {
       const newMarkers = { ...prevMarkers };
       delete newMarkers[id];
       return newMarkers;
-    }
-    );
-  }
-  );
+    });
+  });
 
   return (
     <MapContainer
       className="markercluster-map"
-      center={[
-        0, 0
-      ]}
+      center={[0, 0]}
       zoom={4}
       maxZoom={18}
       minZoom={2}
@@ -67,13 +67,12 @@ export default function App() {
 
       {Object.keys(markers).map((key) => {
         const { latitude, longitude } = markers[key];
-        return (
-          <Marker key={key} position={[latitude, longitude]}></Marker>
-        );
-      }
-      )}
+        return <Marker key={key} position={[latitude, longitude]}></Marker>;
+      })}
 
-
+      {lines.map((line, index) => (
+        <Polyline key={index} positions={line} />
+      ))}
     </MapContainer>
   );
 }
